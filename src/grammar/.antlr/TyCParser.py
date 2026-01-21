2ea85422968715a074008a7bbc1497b498e17839
Bùi Nguyễn Hoàng Thọ <thohnb@gmail.com>
2026-01-21 17:52:18 +0700

    Update TYC.g4 - Lexer [50%]

--- src/grammar/.antlr/TyCParser.py
+++ src/grammar/.antlr/TyCParser.py
@@ -10,7 +10,7 @@ else:
 
 def serializedATN():
     return [
-        4,1,4,5,2,0,7,0,1,0,1,0,1,0,0,0,1,0,0,0,3,0,2,1,0,0,0,2,3,5,0,0,
+        4,1,49,5,2,0,7,0,1,0,1,0,1,0,0,0,1,0,0,0,3,0,2,1,0,0,0,2,3,5,0,0,
         1,3,1,1,0,0,0,0
     ]
 
@@ -24,20 +24,78 @@ class TyCParser ( Parser ):
 
     sharedContextCache = PredictionContextCache()
 
-    literalNames = [  ]
+    literalNames = [ "<INVALID>", "'auto'", "'break'", "'case'", "'continue'", 
+                     "'default'", "'else'", "'float'", "'for'", "'if'", 
+                     "'int'", "'return'", "'string'", "'struct'", "'switch'", 
+                     "'void'", "'while'", "'+'", "'-'", "'*'", "'/'", "'%'", 
+                     "'=='", "'!='", "'<'", "'<='", "'>'", "'>='", "'&&'", 
+                     "'!'", "'||'", "'++'", "'--'", "'='", "'.'", "'['", 
+                     "']'", "'{'", "'}'", "'('", "')'", "';'", "','" ]
 
-    symbolicNames = [ "<INVALID>", "WS", "ERROR_CHAR", "ILLEGAL_ESCAPE", 
-                      "UNCLOSE_STRING" ]
+    symbolicNames = [ "<INVALID>", "AUTO", "BREAK", "CASE", "CONTINUE", 
+                      "DEFAULT", "ELSE", "FLOAT", "FOR", "IF", "INT", "RETURN", 
+                      "STRING", "STRUCT", "SWITCH", "VOID", "WHILE", "ADD", 
+                      "SUB", "MUL", "DIV", "MOD", "EQUAL", "NOTEQUAL", "LESSTHEN", 
+                      "LESSTHENEQUAL", "GREATERTHEN", "GREATERTHENEQUAL", 
+                      "AND", "NOT", "OR", "INCREMENT", "DECREMENT", "ASSIGMENT", 
+                      "ACESS", "LSB", "RSB", "LB", "RB", "LP", "RP", "SEMI", 
+                      "COMMA", "INTLIT", "FLOATLIT", "STRINGLIT", "WS", 
+                      "ERROR_CHAR", "ILLEGAL_ESCAPE", "UNCLOSE_STRING" ]
 
     RULE_program = 0
 
     ruleNames =  [ "program" ]
 
     EOF = Token.EOF
-    WS=1
-    ERROR_CHAR=2
-    ILLEGAL_ESCAPE=3
-    UNCLOSE_STRING=4
+    AUTO=1
+    BREAK=2
+    CASE=3
+    CONTINUE=4
+    DEFAULT=5
+    ELSE=6
+    FLOAT=7
+    FOR=8
+    IF=9
+    INT=10
+    RETURN=11
+    STRING=12
+    STRUCT=13
+    SWITCH=14
+    VOID=15
+    WHILE=16
+    ADD=17
+    SUB=18
+    MUL=19
+    DIV=20
+    MOD=21
+    EQUAL=22
+    NOTEQUAL=23
+    LESSTHEN=24
+    LESSTHENEQUAL=25
+    GREATERTHEN=26
+    GREATERTHENEQUAL=27
+    AND=28
+    NOT=29
+    OR=30
+    INCREMENT=31
+    DECREMENT=32
+    ASSIGMENT=33
+    ACESS=34
+    LSB=35
+    RSB=36
+    LB=37
+    RB=38
+    LP=39
+    RP=40
+    SEMI=41
+    COMMA=42
+    INTLIT=43
+    FLOATLIT=44
+    STRINGLIT=45
+    WS=46
+    ERROR_CHAR=47
+    ILLEGAL_ESCAPE=48
+    UNCLOSE_STRING=49
 
     def __init__(self, input:TokenStream, output:TextIO = sys.stdout):
         super().__init__(input, output)
--- src/grammar/TyC.g4
+++ src/grammar/TyC.g4
@@ -24,8 +24,71 @@ options{
 	language = Python3;
 }
 // TODO: Define grammar rules here
+
 program: EOF;
-// Step 1: Grammar Rules
+
+// NOTE: 1.1 Reserved Keywords = Don't declare with these names
+
+AUTO: 'auto';
+BREAK: 'break';
+CASE: 'case';
+CONTINUE: 'continue';
+DEFAULT: 'default';
+ELSE: 'else';
+FLOAT: 'float';
+FOR: 'for';
+IF: 'if';
+INT: 'int';
+RETURN: 'return';
+STRING: 'string';
+STRUCT: 'struct';
+SWITCH: 'switch';
+VOID: 'void';
+WHILE: 'while';
+
+// NOTE: 1.2 Operators
+ADD: '+';
+SUB: '-';
+MUL: '*';
+DIV: '/';
+MOD: '%';
+EQUAL: '==';
+NOTEQUAL: '!=';
+LESSTHEN: '<';
+LESSTHENEQUAL: '<=';
+GREATERTHEN: '>';
+GREATERTHENEQUAL: '>=';
+// NOTE: 1.3 Logical
+AND: '&&';
+NOT: '!';
+OR: '||';
+//  NOTE: 1.4 Icrement/Decrement
+INCREMENT: '++';
+DECREMENT: '--';
+// NOTE: 1.5 Assigment
+ASSIGMENT: '=';
+ACESS: '.'; // NOTE: Member access
+// NOTE: 1.6 Seperator
+LSB: '[';
+RSB: ']';
+LB: '{';
+RB: '}';
+LP: '(';
+RP: ')';
+SEMI: ';';
+COMMA: ',';
+// Fragments
+fragment INTDIGIT: [0-9]+;
+fragment ESC_SEQ: '\\' [btnfr"\\];
+// intflit
+INTLIT: '-'? INTDIGIT;
+
+FLOATLIT:
+	// match: 1.23e4, 5.67E-2
+	INTDIGIT '.' INTDIGIT ([eE] [+-]? INTDIGIT)?
+	| '-'? INTDIGIT [eE] [+-]? INTDIGIT;
+
+STRINGLIT: '"' ( ~["\\] | ESC_SEQ)* '"';
 
 WS: [ \t\r\n]+ -> skip; // skip spaces, tabs
 // ----------- Stop Here ----------
